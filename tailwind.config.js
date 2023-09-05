be17fe548e33b622e8b9a5d7b765272e754881b7
VictorMattosOW <victor_rm@outlook.com>
2023-09-04 22:01:19 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'body': ['Rubik Iso', 'cursive']
+    }
+  },
+  plugins: [],
+}
+
